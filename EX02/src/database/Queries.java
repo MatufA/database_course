b32cf63edf9d0ff24bcefa75aa6d3d3b3ed61e8f
Yehuda Neumann <33513480+Yuda4@users.noreply.github.com>
2018-08-15 16:53:05 +0300

    Update Queries.java

--- EX02/src/database/Queries.java
+++ EX02/src/database/Queries.java
@@ -3,11 +3,28 @@ package database;
 public class Queries {
 
 	//Create statement
-	public static String CREATE_DATABASE = " ";
-	public static String CREATE_QUEUE_RESERVED_TABLE = "(queue_id,pastient _id, queue_time, doctor_id)";
-	public static String CREATE_DOCTOR_TABLE = "(doctor_id,d_f_name, d_l_name,doctor_type)";
-	public static String CREATE_PATIENTS_TABLE = "(pastient_id, p_f_name, p_l_name, gender)";
-	public static String CREATE_QUEUE_TABLE = "(queue_id,time) ";
+	public static String CREATE_DATABASE = "CREATE DATABASE IF NOT EXISTS mithalim";
+	
+	public static String CREATE_QUEUE_RESERVED_TABLE = "CREATE TABLE IF NOT EXISTS Queue_Reserved (\n"
+								+ "queue_id INT NOT NULL, \n"
+								+ "patient_id INT NOT NULL,\n"
+								+ "queue_time INT NOT NULL,\n"
+								+ "	doctor_id INT NOT NULL\n );";
+	
+	public static String CREATE_DOCTOR_TABLE = "CREATE TABLE IF NOT EXISTS Doctors (\n"
+							+ "doctor_id INT NOT NULL, \n"
+							+ "d_f_name TEXT NOT NULL,\n"
+							+ "d_l_name TEXT NOT NULL,\n"
+							+ "doctor_type text NOT NULL\n );";
+	
+	public static String CREATE_PATIENTS_TABLE = "CREATE TABLE IF NOT EXISTS Patients (\n"
+							+ "patient_id INT NOT NULL, \n"
+							+ "p_f_name TEXT NOT NULL,\n"
+							+ "p_l_name TEXT NOT NULL,\n"
+							+ "gender TEXT NOT NULL\n );";
+	
+	public static String CREATE_QUEUE_TABLE =  "CREATE TABLE IF NOT EXISTS Queue (\n"
+							+ "queue_id INT NOT NULL\n);";
 	
 	//Select statement
 	public static String SELECT_ALL = " ";
